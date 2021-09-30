c20c9905138aabc7ca5bc69f390212f45d3f9b46
Zerogert <zlodey.deadalive@gmail.com>
2021-09-30 19:26:22 +0700

    paginations extensions

---
+++ BusinessLogicLayer.Helpers/Extensions/PaginationExtensions.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLogicLayer.Helpers.Models;
+using System;
+using System.Linq;
+
+namespace ePlaces.DataLayer.Extensions {
+	public static class PaginationExtensions {
+
+		public static IQueryable<T> Pagination<T>(this IQueryable<T> query, int indexPage, int pageSize = 20) {
+			var skipCount = indexPage * pageSize;
+			return query.Skip(skipCount).Take(pageSize);
+		}
+
+		public static PaginationResult<T> ToPaginationResult<T>(this IQueryable<T> query, int indexPage, int pageSize = 20) {
+			var itemsTotal = query.Count();
+			var pagesTotal = (int)Math.Ceiling((double)itemsTotal / pageSize);
+			var data = query.Pagination(indexPage, pageSize).ToList();
+			var paginationResult = new PaginationResult<T>() {
+				Data = data,
+				Index = indexPage,
+				PageSize = pageSize,
+				ItemsTotal = itemsTotal,
+				PagesTotal = pagesTotal
+			};
+			return paginationResult;
+		}
+	}
+}
